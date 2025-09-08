db0f8773dbc47fa31d450121ccf288ff5da10bed
Ihsan <54731682+muhd23ihsan@users.noreply.github.com>
2025-09-08 21:33:00 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// Typing effect
+const texts = ["Graphic Designer", "Photographer", "Creative Thinker"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+    setTimeout(type, 1500); // pause before typing next
+  } else {
+    setTimeout(type, 120);
+  }
+})();
